06eebd795734702f7ab7d9ab9adbb38bf7326d7c
Elli Mattila <elli.mattila@aalto.fi>
2025-12-11 17:43:08 +0200

    Complete unicafe exercise. Refactor components and add table-based statistics.

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average.toFixed(1)} />
+          <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const total = allClicks.length;
+  const average = total ? (good - bad) / total : 0;
+  const positive = total ? (good / total) * 100 : 0;
+
+  const handleGoodClick = () => {
+    setAll(allClicks.concat("L"));
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat("R"));
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat("R"));
+    setBad(bad + 1);
+  };
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+      <br />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  );
+};
+
+export default App;
